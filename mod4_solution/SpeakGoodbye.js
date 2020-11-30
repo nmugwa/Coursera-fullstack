8453dfd2e899172d28c1292e272b69200ea33b18
nmugwa <73949328+nmugwa@users.noreply.github.com>
2020-11-30 10:36:22 +0200

    Create SpeakGoodbye.js

---
+++ mod4_solution/SpeakGoodbye.js
@@ -0,0 +1,15 @@
+// SpeakGoodbye
+
+(function(window) {
+
+var byeSpeaker = {};
+
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
